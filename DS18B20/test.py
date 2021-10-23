af56024086bb774f364090726665d4b3fcc5a161
zaphodikus <zaphodikus@hotmail.com>
2021-10-23 16:21:53 +0100

    fixed temp test, but class is now badly interfaced

--- DS18B20/test.py
+++ DS18B20/test.py
@@ -1,10 +1,16 @@
-#
+# Test the wrapper class for the DS18B20 sensors
 import time
+import os
+import sys
+from pathlib import Path
+sys.path.append(str(Path(os.getcwd()).parent))
 from shared.DS18B20.ds18b20 import DS18B20
 
 if __name__ == "__main__":
     sensors = DS18B20.get_sensors()
+    j = DS18B20()
+    print(sensors)
     while True:
-        for i in range(0, len(sensors)):
-            print(sensors[i].get_sensor_temp())
+        for k in sensors:
+            print(f"addr:{k} = {j.get_sensor_temp(k)}°C")
         time.sleep(1)
