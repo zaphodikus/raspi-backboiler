9a56f1f09d39e86da5ea4433b87c6d642acad1a2
zaphodikus <zaphodikus@hotmail.com>
2021-10-23 16:40:57 +0100

    better object model

--- DS18B20/test.py
+++ DS18B20/test.py
@@ -7,10 +7,12 @@ sys.path.append(str(Path(os.getcwd()).parent))
 from shared.DS18B20.ds18b20 import DS18B20
 
 if __name__ == "__main__":
-    sensors = DS18B20.get_sensors()
-    j = DS18B20()
-    print(sensors)
+    sensorlist = DS18B20.get_sensors()
+    sensors = []
+    for k in sensorlist:
+        sensors.append(DS18B20.new_sensor(k, sensorlist[k]))
+    print(sensorlist)
     while True:
-        for k in sensors:
-            print(f"addr:{k} = {j.get_sensor_temp(k)}°C")
+        for s in sensors:
+            print(f" {s.address()}={s.get_sensor_temp()}°C")
         time.sleep(1)
--- shared/DS18B20/ds18b20.py
+++ shared/DS18B20/ds18b20.py
@@ -6,33 +6,42 @@ import re
 
 
 class DS18B20(object):
-    def __init__(self):
-        self.sensors = self.get_sensors()
+    def __init__(self, address, device_path):
+        self.address = address
+        self.device_path = device_path
 
     @staticmethod
-    def get_sensors():
+    def get_sensor_addresses():
+        """
+        :return: a dict of sensors
+        """
         base_dir = '/sys/bus/w1/devices/'
         sensors = {}
         expr = re.compile('.*28-(.*)')
-        print("Sensors:")
+        #print("Sensors:")
         for df in glob.glob(base_dir + '28*'):
-            print(f"sensor address:{expr.match(df).group(1)}")
+            #print(f"sensor address:{expr.match(df).group(1)}")
             sensors[expr.match(df).group(1)] = df
         return sensors
 
-    def sensor_addresses(self):
-        return self.sensors.keys()
+    def new_sensor(self, address, device_path):
+        print(f"  Creating sensor {address} : {device_path}")
+        return DS18B20(address, device_path)
 
-    def get_sensor_raw(self, address):
-        with open(self.sensors[address] + '/w1_slave', 'r') as f:
+    def address(self):
+        return self.address
+
+    def get_sensor_raw(self):
+        print(f"  Read sensor {self.address} ")
+        with open(self.device_path + '/w1_slave', 'r') as f:
             lines = f.readlines()
             return lines
 
-    def get_sensor_temp(self, address):
-        lines = self.get_sensor_raw(address)
+    def get_sensor_temp(self):
+        lines = self.get_sensor_raw()
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
-            lines = self.get_sensor_raw(address)
+            lines = self.get_sensor_raw()
         equals_pos = lines[1].find('t=')
         if equals_pos != -1:
             temp_string = lines[1][equals_pos + 2:]
